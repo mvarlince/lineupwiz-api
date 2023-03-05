91a41a5c0a725342fd0f306abf174f758a10eec4
Varlince Meran <varlincemeran@gmail.com>
2023-03-05 18:42:28 -0500

    added get, add, update Players functions and routes

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from 'express'
 import cors from 'cors'
-import { addFormation, getFormation } from './src/functions.js'
+import updatePlayer, { addFormation, addPlayer, getFormation, getPlayers } from './src/functions.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => res.send('Api working'))
+
 app.get('/formation', getFormation)
 app.post('/formation', addFormation)
+
+app.get('/players', getPlayers)
+app.post('/players', addPlayer)
+app.patch('/players/:id', updatePlayer)
+
 
 app.listen('4040', () => console.log('listening on port 4040'))
--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import db_connect from "./dbConnect.js";
 
 export async function getFormation(req, res){
@@ -11,18 +12,71 @@ export async function getFormation(req, res){
 }
 
 export async function addFormation(req, res){
-    console.log('running function')
-
-    const formation = req.body
+    const info = req.body
     const db = db_connect()
-    
-    console.log('connected to db')
 
     await db.collection('formations')
-        .insertOne(formation)
+        .insertOne(info)
         .catch( err => {
             res.status(500).send(err)
             return
         })
     res.status(201).send('formation inserted')
 }
+
+export async function getPlayers(req, res){
+    const db = db_connect()
+
+    const content = await db.collection('players')
+        .find({})
+        .toArray()
+
+    res.send(content)
+}
+
+export async function addPlayer(req, res){
+    const data = req.body
+    const db = db_connect()
+
+    await db.collection('players')
+        .insertOne(data)
+        .catch( err => {
+            res.status(500).send(err)
+            return
+        })
+    res.status(201).send('player inserted')
+}
+
+export default async function updatePlayer(req, res){
+    console.log('running function')
+
+    const {id} = req.params
+    const {name, jersey} = req.body
+
+    const db = db_connect()
+    console.log('db connected')
+
+    await db.collection('players')
+        .updateOne(
+            {_id: new ObjectId(id)},
+            { $set: {name, jersey}}
+        )
+        .catch(err => console.log(err))
+    res.status(200).send({ message: 'Player updated' });
+    console.log('done')
+}
+
+
+// export async function getPlayersByFormation(req, res){
+//     const formation = req.body
+
+//     const db = db_connect()
+
+//     await db.collection('players')
+//         .find({})
+//         .then()
+//         .catch( err => {
+//             res.status(500).send(err)
+//             return
+//         })
+// }
